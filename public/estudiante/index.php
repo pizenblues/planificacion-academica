de2d70c2fcc6c63ede04383eafc956274166c6c0
valentina azocar <pizenblues@gmail.com>
2017-02-27 23:55:02 -0400

    horario completo

--- public/estudiante/index.php
+++ public/estudiante/index.php
@@ -13,7 +13,8 @@
     JOIN dia ON horario.horario_dia = dia.dia_id
     JOIN salon ON horario.horario_salon = salon.salon_id
     JOIN bloque ON horario.horario_bloque = bloque.bloque_id
-    WHERE login = '{$login}'";
+    WHERE login = '{$login}'
+    ORDER BY bloque,dia";
 
     $sql = mysql_query($query, $connect);
     $materias = array();
@@ -29,66 +30,43 @@
 <div class="container">
 <?php
     foreach($materias as $result) {
-      $horario[] =  $result["dia_id"]."-".$result["bloque"];
+      $horario[] =  $result["dia_id"].$result["bloque"];
       $materia[] =  $result["materia_nombre"];
     }
     echo print_r($horario);
     echo print_r($materia);
-    $mark = 0;
  ?>
   <div class="page-header">
     <h2>Horario de clases</h2>
   </div>
   <table class="table table-bordered table-custom">
     <tr>
-      <th>Hora</th>
       <th>Lunes</th>
       <th>Martes</th>
       <th>Miercoles</th>
       <th>Jueves</th>
       <th>Viernes</th>
     </tr>
-    <tr>
-      <td>7:00 - 9:00</td>
-      <td>
-        <?php if (in_array("1-A", $horario)){
-          echo $materia[$mark];
-          $mark = ($mark + 1);
-        }?>
-      </td>
-    </tr>
-    <tr>
-      <td>9:00 - 11:00</td>
-      <td id="1-B"></td>
-      <td id="2-B"></td>
-      <td id="3-B"></td>
-      <td id="4-B"></td>
-      <td id="5-B"></td>
-    </tr>
-    <tr>
-      <td>11:00 - 1:00</td>
-      <td id="1-C"></td>
-      <td id="2-C"></td>
-      <td id="3-C"></td>
-      <td id="4-C"></td>
-      <td id="5-C"></td>
-    </tr>
-    <tr>
-      <td>1:00 - 3:00</td>
-      <td id="1-D"></td>
-      <td id="2-D"></td>
-      <td id="3-D"></td>
-      <td id="4-D"></td>
-      <td id="5-D"></td>
-    </tr>
-    <tr>
-      <td>3:00 - 5:00</td>
-      <td id="1-E"></td>
-      <td id="2-E"></td>
-      <td id="3-E"></td>
-      <td id="4-E"></td>
-      <td id="5-E"></td>
-    </tr>
+
+    <?php
+      $bloque = array('A','B','C','D','E');
+      $mark = 0;
+      for ($j=0; $j < 5; $j++) { 
+        echo "<tr>";
+        for ($i=1; $i < 6; $i++) {
+          echo "<td>";
+          if (in_array($i.$bloque[$j], $horario)) {
+            echo $materia[$mark];
+            $mark = ($mark + 1);
+          }else{
+            echo "";
+          }
+          echo "</td>";
+        }
+        echo "</tr>";
+      } 
+    ?>
+    
   </table>
 </div>
 <?php 
---
+++ public/estudiante/test.php
@@ -0,0 +1,7 @@
+<?php
+$algo = "as";
+$bloque = array('A','B','C','D','E');
+ for ($i=0; $i < 5; $i++) { 
+ 	echo $bloque[$i].$algo;
+ }
+ ?>
