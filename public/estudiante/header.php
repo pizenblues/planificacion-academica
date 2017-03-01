6c386e810b7557683f2cbb47a1f8d2547993b282
valentina azocar <pizenblues@gmail.com>
2017-03-01 02:08:04 -0400

    inscripcion

--- public/estudiante/header.php
+++ public/estudiante/header.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <link rel="stylesheet" href="../../modules/bootstrap/dist/css/bootstrap.min.css">
   <link rel="stylesheet" href="../../modules/font-awesome/css/font-awesome.min.css">
-  <link rel="stylesheet" href="../custom.css">
+  <link rel="stylesheet" href="../custom.css?<?php echo time(); ?>">
   <script language="JavaScript" src="../../modules/jquery.min.js" type="text/javascript"></script>
   <title>Estudiante</title>
 </head>
--- public/estudiante/inscribir.php
+++ public/estudiante/inscribir.php
@@ -18,7 +18,19 @@
     while ($i = mysql_fetch_assoc($seccion_result)) {
       $secciones[] = $i;
     }
-
+/*
+    if (!(isset($_POST["seccion"]))) {
+      
+    }else{
+      $materia = $_POST["materia"];
+      $seccion = $_POST["seccion"];
+      if (($materia=="")||($seccion=="")) {
+        echo "mal";
+      }else{
+        echo "bien";
+      }
+    }
+*/
     include("header.php");
     include("navbar.php");
 ?>
@@ -28,15 +40,14 @@
   </div>
   
   <form method="POST">
-    
-    <select id="materia" name="materia" class="selection">
+    <select id="materia" name="materia" class="form-control">
       <option value="">materias</option>
       <?php foreach ($materias as $materia): ?>
           <option value="<?php echo $materia['materia_id'] ?>"><?php echo $materia['materia_nombre'] ?></option>
       <?php endforeach ?>
     </select>
 
-    <select id="seccion" name="seccion" class="selection">
+    <select id="seccion" name="seccion" class="form-control">
       <option value="">secciones</option>
       <?php foreach ($secciones as $seccion): ?>
           <option class="hidden" data-materia="<?php echo $seccion["seccion_materia"] ?>" 
@@ -45,6 +56,9 @@
       <?php endforeach ?>
     </select>
 
+    <button class="btn btn-success">
+      <a class="none" href="../../src/estudiante/inscribir.php">Inscribir</a>
+    </button>
   </form>
 </div>
 
